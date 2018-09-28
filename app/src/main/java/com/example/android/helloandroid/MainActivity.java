a624fefed583c3cdc4ac116f141f6e045df36b1a
Trubius <trubel.gabor@gmail.com>
2018-09-28 16:21:00 +0100

    Refactor clickable content

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.helloandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textAddress = (TextView) findViewById(R.id.address);
+        textAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = textAddress.getText().toString();
+                Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(address));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+        final TextView textPhone = (TextView) findViewById(R.id.phone_number);
+        textPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = textPhone.getText().toString();
+                if (!number.startsWith("tel:")) number = "tel:" + number;
+                Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+                phoneIntent.setData(Uri.parse(number));
+                startActivity(phoneIntent);
+            }
+        });
+
+        final TextView textWeb = (TextView) findViewById(R.id.website);
+        textWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = textWeb.getText().toString();
+                if (!url.startsWith("http://") && !url.startsWith("https://"))
+                    url = "http://" + url;
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
